4d0c7d57365a31428aea993f70f1b7212bbe2083
oalmed <osmedina04@gmail.com>
2025-11-19 18:56:17 -0600

    Codigo QR muestra/acepta caracteres extraños

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
-#include "qrcodegen.hpp"   
+#include <vector>
+#include "qrcodegen.hpp" 
+#include <iomanip>
+#include <clocale>
+#include <windows.h>  
 
 using namespace qrcodegen;
-using namespace std;
 
 int main() {
+	setlocale(LC_ALL, "es_ES.UTF-8");
+	SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
 	system("color f0");
 	
+	std::vector<uint8_t> codigos;
     std::string texto;
     printf("Ingresa un texto: \n");
 	getline(std::cin, texto);
 	
-    QrCode qr = QrCode::encodeText(texto.c_str(), QrCode::Ecc::LOW);
+	for (unsigned char cb : texto) codigos.push_back(static_cast<int>(cb));
+	
+    QrCode qr = QrCode::encodeBinary(codigos, QrCode::Ecc::LOW);
     int size = qr.getSize();
 
     // Quiet zone o margen (módulos blancos alrededor)
@@ -22,10 +31,15 @@ int main() {
             bool dark = false;
             if (x >= 0 && x < size && y >= 0 && y < size) dark = qr.getModule(x, y);
             
-            std::cout << (dark ? std::string(2, static_cast<char>(219)) : std::string(2, static_cast<char>(32)));
+            std::cout << (dark ? u8"\u2588\u2588": "  ");
         }
         std::cout << std::endl;
     }
     return 0;
 }
 
+	/*for (char c : texto) {
+		//unsigned char valor = static_cast<unsigned char>(c);
+        codigos.push_back(static_cast<int>(c));
+        std::cout << c << ", int: " << static_cast<int>(c);
+	}*/
